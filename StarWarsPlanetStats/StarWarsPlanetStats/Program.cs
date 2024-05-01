3cf70144b2c5128a1b3fc124040f15fb9815f9b4
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-05-01 12:24:28 +0300

    Move the main code into StarWarsPlanetStatsApp class.

--- StarWarsPlanetStats/StarWarsPlanetStats/Program.cs
+++ StarWarsPlanetStats/StarWarsPlanetStats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StarWarsPlanetStats.ApiDataAccess;
 using StarWarsPlanetStats.DataTransferObjects;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 namespace StarWarsPlanetStats
@@ -8,27 +9,60 @@ namespace StarWarsPlanetStats
     {
         static void Main(string[] args)
         {
-            string? json = null;
-            const string BaseAddress = "https://swapi.dev/";
-            const string RequestUrl = "api/planets/";
-            // We will use this if the API is down.
-            //IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
-            try
-            {
-                IApiDataReader apiDataReader = new StarWarsApiDataReader();
-                var jsonTask = apiDataReader.Read(BaseAddress, RequestUrl);
-                json = jsonTask.Result;
+            const string ExpectedBaseAddress = "https://swapi.dev/";
+            const string ExpectedRequestUrl = "api/planets/";
+            try 
+            { 
+                StarWarsPlanetStatsApp app = new StarWarsPlanetStatsApp(new StarWarsApiDataReader(),
+                                                                        new MockStarWarsApiDataReader(),
+                                                                    ExpectedBaseAddress, ExpectedRequestUrl);
+                app.Run();
             }
-            catch(HttpRequestException ex) 
+            catch(Exception ex)
             {
-                Console.WriteLine("API request was unsuccessful switching to mock API data.\n"+
-                                   $"Exception message is: {ex.Message}");
-                IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
-                var jsonTask = apiDataReader.Read(BaseAddress, RequestUrl);
-                json = jsonTask.Result;
+                Console.WriteLine( $"An error occured. Exception message is: {ex.Message}.");
             }
-            Root? root = JsonSerializer.Deserialize<Root>(json);
             Console.ReadKey();
+        }
+
+        public class StarWarsPlanetStatsApp
+        {
+            string? json = null;
+            const string ExpectedBaseAddress = "https://swapi.dev/";
+            const string ExpectedRequestUrl = "api/planets/";
+            private readonly IApiDataReader _apiDataReader;
+            private readonly IApiDataReader _mockApiDataReader;
+            private readonly string baseAddress;
+            private readonly string requestUri;
+
+            public StarWarsPlanetStatsApp(IApiDataReader _apiDataReader, IApiDataReader _secondApiDataReader,
+                string baseAddress, string requestUri)
+            {
+                this._apiDataReader = _apiDataReader;
+                this.baseAddress = baseAddress;
+                this.requestUri = requestUri;
+                this._mockApiDataReader = _secondApiDataReader;
+
+            }
+            public void Run()
+            {
+                // We will use this if the API is down.
+                //IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
+                try
+                {
+                    var jsonTask = _apiDataReader.Read(baseAddress, requestUri);
+                    json = jsonTask.Result;
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine("API request was unsuccessful switching to mock API data.\n" +
+                                       $"Exception message is: {ex.Message}");
+
+                    var jsonTask = _mockApiDataReader.Read(ExpectedBaseAddress, ExpectedRequestUrl);
+                    json = jsonTask.Result;
+                }
+                Root? root = JsonSerializer.Deserialize<Root>(json);
+            }
         }
     }
 }
